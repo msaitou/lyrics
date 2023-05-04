b5844e9bdbc192d23eab522714dc5f2fd0aded06
msaitou <peterpansymdromer@gmail.com>
2023-05-05 08:22:16 +0900

    basic認証はenvから読み込み

--- middleware.tsx
+++ middleware.tsx
@@ -1,22 +1,24 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from "next/server";
 
 export const config = {
-  matcher: ['/', '/index', "/memories/edit", "/memories/chord-list"],
-}
+  matcher: ["/", "/index", "/memories/edit", "/memories/chord-list"],
+};
+
+const { BASIC_USER, BASIC_PASS } = process.env;
 
 export function middleware(req: NextRequest) {
-  const basicAuth = req.headers.get('authorization')
-  const url = req.nextUrl
+  const basicAuth = req.headers.get("authorization");
+  const url = req.nextUrl;
 
   if (basicAuth) {
-    const authValue = basicAuth.split(' ')[1]
-    const [user, pwd] = atob(authValue).split(':')
+    const authValue = basicAuth.split(" ")[1];
+    const [user, pwd] = atob(authValue).split(":");
 
-    if (user === 'username' && pwd === 'password') {
-      return NextResponse.next()
+    if (user === BASIC_USER && pwd === BASIC_PASS) {
+      return NextResponse.next();
     }
   }
-  url.pathname = '/api/auth'
+  url.pathname = "/api/auth";
 
-  return NextResponse.rewrite(url)
+  return NextResponse.rewrite(url);
 }
